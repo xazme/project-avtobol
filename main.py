ab0ded323fc86a6f56ec86cb80f696a83543e783
andrey_dremin <sankoa230@gmail.com>
2025-06-03 15:23:06 +0300

    clear comments

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ from app.core.config import settings
 from app.database.db_service import DBService
 from app.storage.storage_service_dependencies import storage_service
 
-# penis
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await DBService.create_tables()
